b13cbac809a1cc1839ad3dba9db364049db74fd8
cazzoo <caz.san@gmail.com>
2016-03-23 11:50:19 +0100

    Updated updater name and initially set remote host server that will handle updates

---
+++ Source/RFUpdater.Patcher/Forms/pForm.cs
@@ -0,0 +1,29 @@
+﻿using RFUpdater.Patcher.Source_files;
+using System;
+using System.Windows.Forms;
+
+namespace RFUpdater.Patcher
+{
+    public partial class pForm : Form
+    {
+        public pForm()
+        {
+            InitializeComponent();
+
+            Globals.pForm = this;
+        }
+
+        private void pForm_Shown(object sender, EventArgs e)
+        {
+            if (Common.IsGameRunning())
+                Common.EnableStart();
+            else
+                Networking.CheckNetwork();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            Starter.Start();
+        }
+    }
+}
