0fa9505e0a9471ba9bc2985cfaf58a2fe2499660
cazzoo <caz.san@gmail.com>
2016-04-15 00:54:34 +0200

    Started new version of RFUpdater that would work handle better mods

---
+++ Source/RFUpdater/LoginWindow.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Gtk;
+
+public partial class LoginWindow: Gtk.Window
+{
+	public LoginWindow () : base (Gtk.WindowType.Toplevel)
+	{
+		Build ();
+		status_login.Push(1, "Waiting for login");
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void login (object sender, EventArgs e)
+	{
+		String return_message = form_validate ();
+		if (return_message != null) {
+			status_login.Push (1, return_message);
+		} else {
+			check_login ();
+		}
+	}
+
+	protected string form_validate()
+	{
+		if (string.Empty == in_login.Text) {
+			return "Field login cannot be empty";
+		}
+		if (string.Empty == in_password.Text) {
+			return "Field password cannot be empty";
+		}
+		return null;
+	}
+
+	protected void check_login() {
+		if(in_login.Text == "caz" && in_password.Text == "caz") {
+			MainWindow main = new MainWindow ();
+			this.Destroy ();
+			main.Show ();
+		}
+	}
+}
---
+++ Source/RFUpdater/MainWindow.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+	public MainWindow () :
+		base (Gtk.WindowType.Toplevel)
+	{
+		this.Build ();
+		Init ();
+	}
+
+	protected void Init()
+	{
+		
+	}
+
+	protected void OnDeleteEvent (object sender, DeleteEventArgs a)
+	{
+		Application.Quit ();
+		a.RetVal = true;
+	}
+
+	protected void QuitApplication (object sender, EventArgs e)
+	{
+		Application.Quit ();
+	}
+}
